71453deee023c30de84006d2bad711d99d34ad69
Kiersty Shoultz <kierstycaekz@live.com>
2020-07-29 20:31:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = "White Mage", "Bard", "Ninja", "Astro"
+  jobs_ffxiv = "White Mage", "Bard", "Ninja", "Astro"
 end
 
 def add_element_to_end_of_array(array, element)
-  jobs << element
+  jobs_ffxiv << "Dancer"
 end
 
 def add_element_to_start_of_array(array, element)
